1e6625cf0e60788e3ce73d3b1ec8bf928db52f06
Temurbek Sabirov <mr.tima5390@gmail.com>
2023-09-13 07:22:41 +0500

    Solve 135_Candy.kt

---
+++ src/main/kotlin/135_Candy.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val s = readln().split(" ").map { it.toInt() }.toIntArray()
+    println(candy(s))
+}
+
+private fun candy(ratings: IntArray): Int {
+    val candies = IntArray(ratings.size) { 1 }
+
+    for (i in 1 until candies.size) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1
+        }
+    }
+
+    var result = candies[candies.size - 1]
+    for (i in candies.size - 2 downTo 0) {
+        if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+            candies[i] = candies[i + 1] + 1
+        }
+        result += candies[i]
+    }
+
+    return result
+}
+
