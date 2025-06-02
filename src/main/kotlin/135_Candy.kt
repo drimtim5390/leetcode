e48c3601089a8bfd8e1cd63bb524fc8ee70541ff
Temurbek Sabirov <mr.tima5390@gmail.com>
2025-06-02 07:40:10 +0500

    Edit 135

--- src/main/kotlin/135_Candy.kt
+++ src/main/kotlin/135_Candy.kt
@@ -1,25 +1,24 @@
 fun main() {
+    fun candy(ratings: IntArray): Int {
+        val candies = IntArray(ratings.size) { 1 }
+
+        for (i in 1 until candies.size) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1
+            }
+        }
+
+        var result = candies[candies.size - 1]
+        for (i in candies.size - 2 downTo 0) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1
+            }
+            result += candies[i]
+        }
+
+        return result
+    }
+
     val s = readln().split(" ").map { it.toInt() }.toIntArray()
     println(candy(s))
 }
-
-private fun candy(ratings: IntArray): Int {
-    val candies = IntArray(ratings.size) { 1 }
-
-    for (i in 1 until candies.size) {
-        if (ratings[i] > ratings[i - 1]) {
-            candies[i] = candies[i - 1] + 1
-        }
-    }
-
-    var result = candies[candies.size - 1]
-    for (i in candies.size - 2 downTo 0) {
-        if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-            candies[i] = candies[i + 1] + 1
-        }
-        result += candies[i]
-    }
-
-    return result
-}
-
